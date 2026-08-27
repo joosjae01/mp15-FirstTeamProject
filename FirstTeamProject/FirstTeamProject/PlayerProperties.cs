d8dc9c8a7b461a44e48b58b714a7bdcf89f3fdd0
JuiceJ <joosjae01@gmail.com>
2026-08-27 14:34:56 +0900

    [Feat] PlayerProperties 추가

---
+++ FirstTeamProject/FirstTeamProject/PlayerProperties.cs
@@ -0,0 +1,14 @@
+﻿public class PlayerProperties<T> : IObserver
+{
+    private T _value;
+    public T Value
+    {
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    public void Publish()
+    {
+
+    }
+}
